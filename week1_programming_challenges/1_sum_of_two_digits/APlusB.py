1688259b5cb7bc408f6aa7bcf4439483e6c84c16
beetee17 <81674501+beetee17@users.noreply.github.com>
2021-03-31 15:47:59 +0800

    Add files via upload

--- week1_programming_challenges/1_sum_of_two_digits/APlusB.py
+++ week1_programming_challenges/1_sum_of_two_digits/APlusB.py
@@ -1,5 +1,9 @@
-import sys
+# python3
 
-input = sys.stdin.read()
-tokens = input.split()
-print(int(tokens[0]) + int(tokens[1]))
+
+def sum_of_two_digits(first_digit, second_digit):
+    return first_digit + second_digit
+
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(sum_of_two_digits(a, b))
